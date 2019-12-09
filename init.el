110189db2ff3ee7f33c7b07359b0257e2826b7e6
greyzhang <greyzhang@126.com>
2019-12-09 21:20:21 +0800

    remove some unused configurations.

--- init.el
+++ init.el
@@ -36,30 +36,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(coffee-tab-width 8)
- '(company-backends
-   (quote
-    (company-bbdb company-nxml company-css company-eclim company-xcode company-cmake company-capf company-files
-                  (company-dabbrev-code company-gtags company-etags company-keywords)
-                  company-oddmuse company-dabbrev)))
- '(company-idle-delay 0)
- '(company-minimum-prefix-length 1)
- '(company-tooltip-idle-delay 0)
- '(compilation-message-face (quote default))
- '(ecb-options-version "2.50")
- '(helm-M-x-fuzzy-match t)
- '(helm-buffers-fuzzy-matching t)
  '(menu-bar-mode nil)
- '(projectile-globally-ignored-buffers nil)
- '(projectile-globally-ignored-file-suffixes (quote (".o")))
- '(projectile-globally-ignored-files (quote ("TAGS" "*.o")))
- '(projectile-tags-command "ctags -Re .")
  '(show-paren-mode t)
  '(tool-bar-mode nil)
- '(which-key-idle-delay 0.3))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Courier New" :foundry "outline" :slant normal :weight normal :height 108 :width normal)))))
+)
