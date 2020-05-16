3fe68fc02533cf81017a7c16d660d214c3d90b62
greyzhang <greyzhang@126.com>
2020-05-16 14:42:14 +0800

    remove some configurations I don't need now.

--- g-config/g-emacs-comm.el
+++ g-config/g-emacs-comm.el
@@ -23,31 +23,9 @@
 (require 'recentf)
 (recentf-mode 1)
 (setq recentf-max-menu-item 100)
-;; (setq default-buffer-file-coding-system 'utf-8-unix)
+
+;; better support for chinese coding
 (set-buffer-file-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-clipboard-coding-system 'utf-8)  
-(setq ansi-color-for-comint-mode t)
-(modify-coding-system-alist 'process "*" 'utf-8)  
-(setq-default pathname-coding-system 'utf-8)  
-(prefer-coding-system 'utf-8)
-(setq default-process-coding-system '(utf-8 . utf-8))  
-(setq locale-coding-system 'utf-8)
-(setq file-name-coding-system 'utf-8) 
-(setq default-buffer-file-coding-system 'utf-8)  
-(setq slime-net-coding-system 'utf-8-unix)
-
-;;; =========
-;;; Note that if you save a heap image, the character
-;;; encoding specified on the command line will be preserved,
-;;; and you won't have to specify the -K utf-8 any more.
-;;; (setq inferior-lisp-program "/usr/local/bin/ccl64 -K utf-8")
-
-(setq inferior-lisp-program "dx86cl64 -K utf-8")
-(setq inferior-lisp-program "sbcl -K utf-8")
 
 ;;
 (provide 'g-emacs-comm)
