cd1c1ae39ff3d245b2585f5ec6d260afeeb58343
greyzhang <greyzhang@126.com>
2020-05-24 17:31:48 +0800

    Remove some configurations generated during test.

--- init.el
+++ init.el
@@ -43,10 +43,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ac-auto-show-menu 0.0)
- '(ac-auto-start 1)
- '(ac-delay 0.0)
- '(company-auto-complete nil)
  '(company-backends
    (quote
     ((company-dabbrev-code company-gtags company-etags company-keywords company-clang))))
