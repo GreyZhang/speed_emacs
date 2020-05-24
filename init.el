ecb4a7791f1c75fe27bb68c8c1d8698312e4401d
greyzhang <greyzhang@126.com>
2020-05-24 17:21:43 +0800

    Add company and related docs.

--- init.el
+++ init.el
@@ -16,6 +16,7 @@
 		      iedit
 		      autopair
 		      which-key
+		      company
                       ) "Default packages")
 
  (setq package-selected-packages my/packages)
@@ -35,12 +36,22 @@
 (add-to-list 'load-path "~/.emacs.d/g-config")
 (require 'g-config)
 
+;; configurations for new package try.
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ac-auto-show-menu 0.0)
+ '(ac-auto-start 1)
+ '(ac-delay 0.0)
+ '(company-auto-complete nil)
+ '(company-backends
+   (quote
+    ((company-dabbrev-code company-gtags company-etags company-keywords company-clang))))
+ '(company-idle-delay 0)
+ '(company-minimum-prefix-length 1)
  '(menu-bar-mode nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil))
